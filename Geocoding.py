33c79cb3ae29dbb588d71c7d776122be83f32ae0
alessandrofesti <alefesti.work@gmail.com>
2020-01-17 12:30:13 +0100

    Geocoding.py python version

--- Geocoding.py
+++ Geocoding.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
-
-
 import os
 import pandas as pd
 import numpy as np 
@@ -12,11 +9,6 @@ import json
 
 os.chdir('...')
 dataset = pd.read_csv("elenco_esercizi_commercio_in_sede_fissa_anno_2018.csv", sep = ';', header='infer', encoding='latin-1')
-dataset.head()
-
-
-# In[13]:
-
 
 dataset['quartiere_settore'] = dataset.ESERCIZIO_VIA+'  '+dataset.ESERCIZIO_CIVICO+' '+dataset.QUARTIERE+' Bologna'
 dataset['lat'] = float
@@ -35,16 +27,6 @@ for i in range(len(dataset)):
     except:
         dataset.iat[i,29] = np.nan
         dataset.iat[i,30] = np.nan
-
-
-# In[15]:
-
-
-dataset.tail()
-
-
-# In[16]:
-
 
 dataset.to_excel('geocoded.xlsx')
 
